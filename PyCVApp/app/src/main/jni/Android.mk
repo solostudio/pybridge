c7160ea7f5a8c3690a3cedb30a82c6c64eaedcab
dkwang911 <solobear@foxmail.com>
2020-08-25 00:29:59 +0800

    python 3.7.6

--- PyCVApp/app/src/main/jni/Android.mk
+++ PyCVApp/app/src/main/jni/Android.mk
@@ -3,8 +3,7 @@ LOCAL_PATH := $(call my-dir)
 # Python-for-Android paths
 
 PYTHON_FOR_ANDROID_PATH := $(LOCAL_PATH)/../../../../../python-for-android
-GUEST_PYTHON_PATH := $(PYTHON_FOR_ANDROID_PATH)/build/other_builds/python3
-PYTHON_PATH := $(GUEST_PYTHON_PATH)/armeabi-v7a__ndk_target_21/python3
+PYTHON_PATH := $(PYTHON_FOR_ANDROID_PATH)/libs/arm64-v8a
 
 
 # Build libpybridge.so
@@ -13,14 +12,14 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := pybridge
 LOCAL_SRC_FILES := pybridge.c
 LOCAL_LDLIBS := -llog
-LOCAL_SHARED_LIBRARIES := python3.8m
+LOCAL_SHARED_LIBRARIES := python3.7m
 include $(BUILD_SHARED_LIBRARY)
 
 
-# Include libpython3.8m.so
+# Include libpython3.7m.so
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := python3.8m
-LOCAL_SRC_FILES := $(PYTHON_PATH)/android-build/libpython3.8m.so
+LOCAL_MODULE    := python3.7m
+LOCAL_SRC_FILES := $(PYTHON_PATH)/libpython3.7m.so
 LOCAL_EXPORT_CFLAGS := -I $(PYTHON_PATH)/Include
 include $(PREBUILT_SHARED_LIBRARY)
